b571c2372c9feda092a1ad98669819baae20c175
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-05-27 14:08:38 -0700

    Add files via upload

---
+++ ChessGUI/ChessGUI/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace ChessGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChessPieceSelectLabel = new System.Windows.Forms.Label();
+            this.ChessPieceComboBox = new System.Windows.Forms.ComboBox();
+            this.ChessBoardPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ChessPieceSelectLabel
+            // 
+            this.ChessPieceSelectLabel.AutoSize = true;
+            this.ChessPieceSelectLabel.BackColor = System.Drawing.Color.Black;
+            this.ChessPieceSelectLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ChessPieceSelectLabel.ForeColor = System.Drawing.Color.White;
+            this.ChessPieceSelectLabel.Location = new System.Drawing.Point(12, 18);
+            this.ChessPieceSelectLabel.Name = "ChessPieceSelectLabel";
+            this.ChessPieceSelectLabel.Size = new System.Drawing.Size(475, 15);
+            this.ChessPieceSelectLabel.TabIndex = 0;
+            this.ChessPieceSelectLabel.Text = "Select a type of chess piece and then click on the grid. I will show you all lega" +
+    "l moves";
+            // 
+            // ChessPieceComboBox
+            // 
+            this.ChessPieceComboBox.BackColor = System.Drawing.Color.Silver;
+            this.ChessPieceComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ChessPieceComboBox.FormattingEnabled = true;
+            this.ChessPieceComboBox.Items.AddRange(new object[] {
+            "King",
+            "Queen",
+            "Knight",
+            "Rook",
+            "Bishop"});
+            this.ChessPieceComboBox.Location = new System.Drawing.Point(493, 15);
+            this.ChessPieceComboBox.Name = "ChessPieceComboBox";
+            this.ChessPieceComboBox.Size = new System.Drawing.Size(121, 23);
+            this.ChessPieceComboBox.TabIndex = 1;
+            // 
+            // ChessBoardPanel
+            // 
+            this.ChessBoardPanel.BackColor = System.Drawing.Color.Silver;
+            this.ChessBoardPanel.Location = new System.Drawing.Point(63, 55);
+            this.ChessBoardPanel.Name = "ChessBoardPanel";
+            this.ChessBoardPanel.Size = new System.Drawing.Size(500, 500);
+            this.ChessBoardPanel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ChessGUI.Properties.Resources.chess;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(626, 577);
+            this.Controls.Add(this.ChessBoardPanel);
+            this.Controls.Add(this.ChessPieceComboBox);
+            this.Controls.Add(this.ChessPieceSelectLabel);
+            this.Name = "Form1";
+            this.Text = "Chess Legal Moves";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ChessPieceSelectLabel;
+        private System.Windows.Forms.ComboBox ChessPieceComboBox;
+        private System.Windows.Forms.Panel ChessBoardPanel;
+    }
+}
+
---
+++ ChessGUI/ChessGUI/Form1.cs
@@ -0,0 +1,113 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* * * * * * * * * * *
+ * Warren Peterson * *
+ * This is my own work
+ * CST-227 5/27/2021 *
+ * Chess Board App * *
+ * * * * * * * * * * */
+
+namespace ChessGUI
+{
+    public partial class Form1 : Form
+    {
+        static public Board board = new Board(8);
+        public Button[,] btnGrid = new Button[board.Size, board.Size];
+        public Form1()
+        {
+            InitializeComponent();
+            populateGrid();
+        }
+        public void populateGrid()
+        {
+            // This function will fill the panel control with buttons
+            int buttonSize = ChessBoardPanel.Width / board.Size; // Calculate the width of each button on the Grid
+            ChessBoardPanel.Height = ChessBoardPanel.Width; // Set the grid to be square
+
+            // Nested Loop. Create buttons and place them in the Panel
+            for (int r = 0; r < board.Size; r++)
+            {
+                for (int c = 0; c < board.Size; c++)
+                {
+                    btnGrid[r, c] = new Button();
+
+                    // Make each button square
+                    btnGrid[r, c].Width = buttonSize;
+                    btnGrid[r, c].Height = buttonSize;
+
+                    btnGrid[r, c].Click += Grid_Button_Click; // Add the same click event to each button.
+                    ChessBoardPanel.Controls.Add(btnGrid[r, c]); // Place the button on the Panel
+                    btnGrid[r, c].Location = new Point(buttonSize * r, buttonSize * c); // Position it in x,y
+
+                    // for testing purposes, Remove later.
+                    //btnGrid[r, c].Text = r.ToString() + "|" + c.ToString();
+
+                    // The Tag attribute will hold the row and column number in a string
+                    btnGrid[r, c].Tag = new Point(r, c);
+                }
+            }
+
+
+        }
+
+        private void Grid_Button_Click(object sender, EventArgs e)
+        {
+            // Get the row and column number of the button just clicked.
+            Button clickedButton = (Button)sender;
+            Point location = (Point)clickedButton.Tag;
+            int x = location.X;
+            int y = location.Y;
+
+            // Run a help function to label all legal moves for this place.
+            Cell currentCell = board.theGrid[x, y];
+            board.MarkNextLegalMoves(currentCell, ChessPieceComboBox.Text);
+
+            // Reset the background color of all buttons to the default color.
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+
+                    btnGrid[i, j].BackColor = default(Color);
+                }
+            }
+            // Set the background color of the clicked button to something different
+            (sender as Button).BackColor = Color.Cornsilk;
+
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    btnGrid[i, j].Text = "";
+                    if (board.theGrid[i, j].LegalNextMove)
+                        btnGrid[i, j].Text = "Legal";
+                    if (board.theGrid[i, j].CurrentlyOccupied)
+                        btnGrid[i, j].Text = ChessPieceComboBox.Text;
+                    board.theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+        }
+
+        public void UpdateButtonLabels()
+        {
+            for (int r = 0; r < board.Size; r++)
+            {
+                for (int c = 0; c < board.Size; c++)
+                {
+                    btnGrid[r, c].Text = "";
+                    if (board.theGrid[r, c].CurrentlyOccupied) btnGrid[r, c].Text = ChessPieceComboBox.Text;
+                    if (board.theGrid[r, c].LegalNextMove) btnGrid[r, c].Text = "Legal";
+                }
+            }
+        }
+    }
+}
