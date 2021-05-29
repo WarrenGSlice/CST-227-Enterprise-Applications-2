e1e0eaabbc896f251ce2c934df565e4bc374cb5f
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-05-29 14:49:20 -0700

    Add files via upload

---
+++ Milestone2/Milestone1/Board.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* * * * * * * * * * * *
+ * Warren Peterson * * *
+ * CST-227 5/29/2021 * *
+ * * * Milestone 2 * * *
+ * This is my own work *
+ * Console Minesweeper *
+ * * * * * * * * * * * */
+
+namespace Milestone2
+{
+    class Board
+    {
+        // Variables
+        public int Size { get; private set; } // Variable to hold board size
+        public Cell[,] grid {get; private set;}// Variable to hold the cell grid in a 2-Dimensional Array of the cell object type
+        private static int Difficulty { get; set; } // Variable to hold the difficulty level
+        
+        // Empty Constructor
+        public Board()
+        {
+            Size = 1;
+            Difficulty = 15; // 15% of board is bombs to start
+            grid = new Cell[Size, Size];
+        }// end of method
+
+        // Constructor that passes one argument
+        public Board(int size)
+        {
+            Size = size;
+            Difficulty = 15; // 15% of board is bombs to start
+            grid = new Cell[Size, Size];
+            SetupLiveNeighbors();
+        }// end of method
+
+        // Method to set up neighbor cells
+        public void SetupLiveNeighbors()
+        {
+            // Variables
+            int squares = (int)Math.Pow(Size, 2); // Variable to hold the cell number
+            int totalBombs = (int)Math.Ceiling((decimal)squares * ((decimal)Difficulty / 100)); // Variable to hold total amount of bombs based on difficulty
+
+            // Create an array to determine which cells have bombs
+            Random rand = new Random();
+            bool[] mineArray = new bool[squares];
+
+            // Create an Array to hold a new array of the sorted bomb array
+            Double[] sortArray = new Double[squares];
+            for (int i = 0; i < sortArray.Length; i++)
+            {
+                sortArray[i] = rand.NextDouble();
+            } // end of for loop
+                
+            // Filled the mineArray with bombs based on difficulty
+            for (int j = 0; j < squares; j++)
+            {
+                mineArray[j] = j < totalBombs;
+            } // end of for loop
+
+            // Sorts the placement of mines
+            Array.Sort(sortArray, mineArray);
+
+            // Iterates through the array and creates the Cell objects
+            int bombIndex = 0;
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    grid[row, col] = new Cell(col, row, false, mineArray[bombIndex], 0);
+                    bombIndex++;
+                } // end of for loop
+            } // end of for loop
+            // Make a second pass over the Grid and calculate all live neighbors for each cell
+            CalculateLiveNeighbors();
+        }// end of method
+
+        // Method that calculates the number on Bomb Neighbor Cells
+        public void CalculateLiveNeighbors()
+        {
+            // Variables
+            int xBombs; // Variable to represent how many bombs around a cells x-axis
+            int yBombs; // Variable to represent how many bombs around a cells y-axis
+            for (xBombs = 0; xBombs < Size; xBombs++)
+            {
+                for (yBombs = 0; yBombs < Size; yBombs++)
+                {
+                    // Cell Right 1
+                    if (fixError(xBombs + 1, yBombs) && grid[xBombs + 1, yBombs].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Right 1 & Up 1
+                    if (fixError(xBombs + 1, yBombs + 1) && grid[xBombs + 1, yBombs + 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Right 1 & Down 1
+                    if (fixError(xBombs + 1, yBombs - 1) && grid[xBombs + 1, yBombs - 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Left 1 & Down 1
+                    if (fixError(xBombs - 1, yBombs - 1) && grid[xBombs - 1, yBombs - 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Up 1
+                    if (fixError(xBombs, yBombs + 1) && grid[xBombs, yBombs + 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Left 1
+                    if (fixError(xBombs - 1, yBombs) && grid[xBombs - 1, yBombs].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Left 1 & Up 1
+                    if (fixError(xBombs - 1, yBombs + 1) && grid[xBombs - 1, yBombs + 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                    // Cell Down 1
+                    if (fixError(xBombs, yBombs - 1) && grid[xBombs, yBombs - 1].HasBomb)
+                    {
+                        grid[xBombs, yBombs].LiveNeighbor++;
+                    }
+                }// end of for loop
+            }// end of for loop
+        }// end of method
+
+        // Method to fix the index out of bounds error
+        public Boolean fixError(int x, int y)
+        {
+            return (x >= 0 && x < Size && y >= 0 && y < Size);
+        }// end of method
+    }
+}
---
+++ Milestone2/Milestone1/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+/* * * * * * * * * * * *
+ * Warren Peterson * * *
+ * CST-227 5/29/2021 * *
+ * * * Milestone 2 * * *
+ * This is my own work *
+ * Console Minesweeper *
+ * * * * * * * * * * * */
+
+namespace Milestone2
+{
+    class Program
+    {
+        public static Board board;
+        static void Main(string[] args)
+        {
+            // Variables
+            int size = -1; // sets the variable size to default value of -1
+            bool endGame = false; // varible to see if game is over or not -- #2
+            
+            Console.Out.WriteLine("Welcome to Minesweeper! ");
+
+            // Sets the grid and size
+            while (size == -1)
+            {
+                Console.Out.WriteLine("Please enter the desired size of the grid: ");
+                size = GetInput();
+                // check for invalid input
+                if (size == -1) 
+                {
+                    continue; 
+                } 
+                board = new Board(size);
+
+                // Play game
+                Console.Clear();
+            } // end of while loop
+
+            // Game Loop that:
+            // 1. Ask the user for a row and column number
+            // 2. If the grid contains a bomb at the chosen cell, then set end game condition to true
+            // 3. If all non-bomb cells are revealed, then set end game to true
+            // 4. Print the grid
+            while (!endGame)
+            {
+                // 1. Ask the user for a row and column number
+                Console.Out.WriteLine("Enter a Row Number");
+                int row = GetInput();
+                Console.Out.WriteLine("Enter a Column Number");
+                int col = GetInput();
+                // Check for invalid input entries
+                if (row < 0 || col < 0 || row >= size || col >= size)
+                {
+                    Console.Out.WriteLine("A Row or Column number goes beyond the scope of the board. Please try again.");
+                    continue;
+                }
+
+                // 2. If the grid contains a bomb at the chosen cell, then set end game condition to true
+                if (board.grid[row, col].HasBomb)
+                {
+                    Console.Out.WriteLine("You hit a Bomb! KABOOM! Game Over!");
+                    endGame = true;
+                    continue;
+                }
+                else
+                {
+                    // 3. If all non-bomb cells are revealed, then set end game to true
+                    if (WonGame())
+                    {
+                        Console.Out.WriteLine("Congrats! You Won Minesweeper and have been promoted to Nuclear Waste Sweeper!");
+                        endGame = true;
+                        continue;
+                    }
+                }
+                // If no end game conditions are found, set square to visited and continue
+                board.grid[row, col].Visited = true;
+
+                // 4. Print the grid
+                PrintBoardDuringGame();
+            } // end of while loop
+        }// end of method
+
+        // Helper method to seek user input
+        public static int GetInput()
+        {
+            // Varibles
+            int input; // declares variable to type integer for holding user input
+            if (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.Out.WriteLine("Please enter a valid number");
+                return -1;
+            }
+            return input;
+        }// end of method
+
+        // Method to create game board in correct format
+        public static void PrintBoardDuringGame()
+        {
+            // Variables
+            int rows = board.grid.GetLength(0); // variable to hold row length, set to 0
+            int cols = board.grid.GetLength(1); // variable to hold column length, set to 1
+            int i; // variable to hold index value of column header
+            int row; // variable to hold new row lines
+            int col; // variable to hold new column lines
+            // Prints the Column Headers
+            for ( i = 0; i < cols; i++)
+            {
+                if (i == 0) Console.Out.Write("|");
+                Console.Out.Write(string.Format(" {0} |", i));
+            } // end of for loop
+            // Prints new line to the game grid
+            SeparateLines(cols);
+            for (row = 0; row < rows; row++)
+            {
+                for (col = 0; col < cols; col++)
+                {
+                    Console.Out.Write("| ");
+                    if (board.grid[row, col].Visited)
+                    {
+                        if (board.grid[row, col].LiveNeighbor > 0)
+                        {
+                            Console.Out.Write(board.grid[row, col].LiveNeighbor);
+                        }
+                        else
+                        {
+                            Console.Out.Write(" ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Out.Write("?");
+                    }
+                    Console.Out.Write(" ");
+                    // Prints end of row numbers
+                    if (col == cols - 1) Console.Out.Write(string.Format("| {0} ", row));
+                } // end of for loop
+                SeparateLines(cols); // next line
+            } // end of for loop
+        }// end of method
+
+        // Helper method to separate lines in a nice looking format
+        private static void SeparateLines(int number)
+        {
+            Console.Out.Write("\n|");
+            for (int i = 0; i < number; i++)
+            {
+                Console.Out.Write("---|");
+            }
+            Console.Out.Write("\n");
+        }// end of method
+
+        // Method to determine if game has been won
+        public static bool WonGame()
+        {
+            int rows = board.grid.GetLength(0);// variable to hold row length, set to 0
+            int cols = board.grid.GetLength(1);// variable to hold column length, set to 1
+            bool isVisited = false; // Variable to hold if any squares are left unvisited
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (!board.grid[i, j].Visited && !board.grid[i, j].HasBomb) isVisited = true;
+                } // end of for loop
+            } // end of for loop
+            return !isVisited;
+        }// end of method
+    } // end of class
+} // end of namespace
