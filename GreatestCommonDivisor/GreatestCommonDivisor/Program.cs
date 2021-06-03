83462aae04da2b053ca3fffbb1ee470eb8c90919
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-06-03 12:22:43 -0700

    Add files via upload

---
+++ GreatestCommonDivisor/GreatestCommonDivisor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/* * * * * * * * * * *
+ * Warren Peterson * *
+ * This is my own work
+ * CST-227 6/3/2021  *
+ * Activity 3  * * * *
+ * GreatestCommonDivis
+ * * * * * * * * * * */
+
+namespace GreatestCommonDivisor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number1 = 400;
+            int number2 = 85;
+            int answer = gcd(number1, number2);
+            Console.Out.WriteLine("The gcd of {0} and {1} is {2}", number1, number2, answer);
+            Console.ReadLine();
+        }
+
+        public static int gcd(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                Console.Out.WriteLine("Not yet. Remainder is {0}", n1 % n2);
+                return gcd(n2, n1 % n2);
+            }
+        }
+    }
+}
