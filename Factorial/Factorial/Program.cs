4e6b1e8aa1046e7d179aa090cfa0754a1aedccd3
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-06-03 12:22:17 -0700

    Add files via upload

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/* * * * * * * * * * *
+ * Warren Peterson * *
+ * CST-227 6/3/2021  *
+ * This is my own work
+ * Activity 3  * * * *
+ * Factorial Exercise 
+ * * * * * * * * * * */
+
+namespace Factorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int startingNumber = 6;
+            Console.Out.WriteLine(Factorial(startingNumber));
+            Console.ReadLine();
+        }
+
+        public static int Factorial(int x)
+        {
+            Console.Out.WriteLine("x is {0}", x);
+            if (x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
