5900e3a32418b17f8d09ce83e47f04cb5ee07a6a
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-06-03 12:21:48 -0700

    Add files via upload

---
+++ CountToOne/CountToOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/* * * * * * * * * * * * * *
+ * * Warren Peterson * * * *
+ * * CST-227 *  6/3/2021 * *
+ * This is my own work * * *
+ * * * Activity 3  * * * * *
+ * * * Count To One  * * * *
+ * * * * * * * * * * * * * */
+
+namespace CountToOne
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Please enter an integer. I will do some math and eventually arrive at 1.");
+            int startingNumber = int.Parse(Console.ReadLine());
+            int x = CountToOne(startingNumber);
+            Console.ReadLine();
+        }
+
+        public static int CountToOne(int n)
+        {
+            Console.Out.WriteLine("N is {0}", n);
+
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Out.WriteLine("N is even. Divide by 2");
+                    return CountToOne(n / 2);
+                }
+                else
+                {
+                    Console.Out.WriteLine("N is odd. Add 1");
+                    return CountToOne(n + 1);
+                }
+            }
+        }
+    }
+}
